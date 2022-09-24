e66642537a1a809ea58a807d217f4510d5bc198b
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-09-24 19:52:41 +0300

    Caesar Exercise

---
+++ caesarExe.js
@@ -0,0 +1,23 @@
+function caesar(inputWord) {
+    let letters = inputWord.split('');
+    let arrayOfNums = [];
+
+    for (let letter of letters) {
+        arrayOfNums.push(letter.charCodeAt());
+    }
+
+    let newNums = [];
+    
+    for (let num of arrayOfNums) {
+        newNums.push(num + 3);
+    };
+
+    let result = [];
+    for (let newNum of newNums) {
+        result.push(String.fromCharCode(newNum));
+    }
+
+    console.log(result.join(''));
+}
+
+caesar('attack');
